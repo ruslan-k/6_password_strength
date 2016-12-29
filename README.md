a5d60019e03cb27b0c546b2705d8f4413eb63e10
Ruslan Kurbanov <wkurbanov@gmail.com>
2016-12-29 21:40:01 +0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # 6_password_strength
+
+Скрипт определяет сложность вашего пароля.
+
+## Применение:
+
+    python3.5 password_strength.py blacklist
+
+Где 'blaclist' - путь к файлу со списком наиболее встречаемых пароле, каждый с новой строки.
+Список можно скачать  [здесь](https://github.com/danielmiessler/SecLists/tree/master/Passwords)
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,47 @@
+import re
+import string
+import sys
 
 
-def get_password_strength(password):
-    pass
+def input_password():
+    password = None
+    while not password:
+        password = input('Введите ваш пароль: ')
+        if not password:
+            print("Введен пустой пароль, попробуй снова.")
+        elif re.search('[а-яА-Я]', password):
+            print('Недопустимы кириллические символы в пароле. Введите пароль снова')
+            password = None
+        else:
+            return password
+
+def get_blacklisted_passwords(filepath):
+    try:
+        with open(filepath) as infile:
+            blacklist = infile.read().split("\n")
+            return blacklist
+    except FileNotFoundError:
+        sys.exit('Неверный путь к файлу словаря с наиболее частыми паролями.')
+
+def get_password_strength(password, blacklist):
+    score = 0
+    while not score:
+        if password in blacklist:
+            print("Пароль находится в черном списке, попробуйте другой.")
+            password = input_password()
+        else:
+            score += int(len(password) >= 4) + int(len(password) >= 8) + int(len(password) >= 14)
+            score += 3 * int(password.lower() != password or password.upper() != password)
+            score += int(len([char for char in password if char in string.digits]) > 0) * 2
+            score += int(len([char for char in password if char in string.punctuation]) > 0) * 2
+    return "Оценка степени защиты пароля - {}/10".format(score)
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        blacklist = get_blacklisted_passwords(sys.argv[1])
+        password = input_password()
+        password_strength = get_password_strength(password, blacklist)
+        print(password_strength)
+    except IndexError:
+        print('Не указан путь к файлу')
